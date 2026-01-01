e450ffb8874d0bf61abea314c733bb101b8b0dc9
MD. Fazle Rabbi Riyad <riyad.cse27@gmail.com>
2026-01-01 12:01:15 +0000

    Solved H-Index problem

--- array/H-Index.py
+++ array/H-Index.py
@@ -1,1 +1,22 @@
-print('Hello world!!')
+class Solution:
+    def __init__(self, arr):
+        self.list = arr
+        n = len(self.list)
+
+        for i in range(n):
+            for j in range(0, n-i-1):
+                if self.list[j] > self.list[j+1]:
+                    self.list[j], self.list[j+1] = self.list[j+1], self.list[j]
+
+        h = 0
+        for i in range(n):
+            if self.list[i] >= n - i:
+                h = n - i
+                break
+
+        self.h = h
+        print(self.h) 
+
+
+obj = Solution([3,0,6,1,5])
+
